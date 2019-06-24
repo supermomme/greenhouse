675140c583750856a92fd46689d0a5fbdc4d68fd
Momme Jürgensen <momme@juergensen.me>
2019-06-24 20:32:14 +0200

    add mqtt and first sensor

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
+var mosquitto = require('mqtt')
 var five = require("johnny-five");
+
 var board = new five.Board();
+var mqtt  = mosquitto.connect('mqtt://192.168.1.232', {
+  username: 'ubuntu',
+  password: '1234'
+})
 
 board.on("ready", function() {
-  var led = new five.Led(13);
-  led.blink(500);
 
   // This requires OneWire support using the ConfigurableFirmata
   var thermometer = new five.Thermometer({
     controller: "DS18B20",
-    pin: 2
+    pin: 2,
+    freq: 1000
   });
 
-  thermometer.on("change", function() {
-    console.log(this.celsius + "°C");
-    // console.log("0x" + this.address.toString(16));
+  thermometer.on("data", function({celsius}) {
+    console.log(`${celsius} celsius`)
+    mqtt.publish(`greenhouse/temperature/indoor`, celsius.toString())
   });
 });
