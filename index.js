06bbd9bd7de09a146c47838cf1e273e6db72db50
Momme Jürgensen <momme@juergensen.me>
2019-06-28 10:53:16 +0100

    moved some sensors from ard to pi

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
-var mosquitto = require('mqtt')
-var five = require("johnny-five");
+const mosquitto = require('mqtt')
+const five = require('johnny-five')
+const dht11 = require('node-dht-sensor')
+const ds18b20 = require('ds18b20')
 
-var board = new five.Board();
-var mqtt  = mosquitto.connect('mqtt://192.168.1.232', {
+//const board = new five.Board();
+const mqtt  = mosquitto.connect('mqtt://192.168.1.232', {
   username: 'ubuntu',
   password: '1234'
 })
 
-board.on("ready", function() {
+const outdoorSensorId = '28-0315747704ff'
+const sensorCrawlInterval = 60000
+
+//Indoor Humidity and Temperature AND Outdoor Temperature
+setInterval(function() {
+  dht11.read(11, 17, function(err, temperature, humidity) {
+    if (err) throw err;
+    //console.log(temperature, humidity)
+    mqtt.publish(`greenhouse/temperature/indoor`, temperature.toFixed(1).toString())
+    mqtt.publish(`greenhouse/humidity/indoor`, humidity.toFixed(1).toString())
+  })
+
+  let outdoorTemperature = ds18b20.temperatureSync(outdoorSensorId)
+  //console.log(outdoorTemperature)
+  mqtt.publish(`greenhouse/temperature/outdoor`, outdoorTemperature.toString())
+}, sensorCrawlInterval)
+
+
+//Comming Soon
+
+//Soil Moisture sensor via johnny five
+/*board.on("ready", function() {
 
   // This requires OneWire support using the ConfigurableFirmata
   var thermometer = new five.Thermometer({
@@ -17,7 +40,7 @@ board.on("ready", function() {
   });
 
   thermometer.on("data", function({celsius}) {
-    console.log(`${celsius} celsius`)
-    mqtt.publish(`greenhouse/temperature/indoor`, celsius.toString())
+    //console.log(`${celsius} celsius`)
+    //mqtt.publish(`greenhouse/temperature/outdoor`, celsius.toString())
   });
-});
+});*/
