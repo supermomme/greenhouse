aff04aafbb48978c9bcdcf3352a96a5b3db89bd6
Momme Jürgensen <momme@juergensen.me>
2019-07-03 13:38:27 +0200

    add soil moisture sensors

--- README.md
+++ README.md
@@ -4,13 +4,12 @@ Some sensors are read out and send to an mqtt broker for further processing.
 ## Wiring
 * The DHT11 sensor on **GPIO 17**
 * The DS18B20 sensor is read via **1-Wire**
-* Capacitive Soil Moisture sensors are read via johnny five (Arduino Uno) (Comming Soon)
+* Capacitive Soil Moisture sensors are read via johnny five (Analog Pins of an Arduino Uno)
 
 ## MQTT
 The Sensors are send to an mqtt broker in following topics:
 * indoor temperature (DHT11) = **greenhouse/temperature/indoor**
 * indoor humidity (DHT11) = **greenhouse/humidity/indoor**
 * outdoor temperature (DS18B20) = **greenhouse/temperature/outdoor**
-* *Comming Soon*
-  * soil moisture = **greenhouse/moisture/soil/*n***
-    * average with **greenhouse/moisture/soil/average**
+* soil moisture = **greenhouse/moisture/soil/*n***
+  * average with **greenhouse/moisture/soil/average**
--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const mosquitto = require('mqtt')
 const five = require('johnny-five')
-const dht11 = require('node-dht-sensor')
-const ds18b20 = require('ds18b20')
+//const dht11 = require('node-dht-sensor')
+//const ds18b20 = require('ds18b20')
 
-//const board = new five.Board();
+const board = new five.Board();
 const mqtt  = mosquitto.connect('mqtt://192.168.1.232', {
   username: 'ubuntu',
   password: '1234'
 })
 
 const outdoorSensorId = '28-0315747704ff'
-const sensorCrawlInterval = 60000
+const sensorCrawlInterval = 15000
 
 //Indoor Humidity and Temperature AND Outdoor Temperature
 setInterval(function() {
@@ -27,20 +27,34 @@ setInterval(function() {
 }, sensorCrawlInterval)
 
 
-//Comming Soon
-
 //Soil Moisture sensor via johnny five
-/*board.on("ready", function() {
+board.on("ready", function() {
+  let latestData = [0, 0, 0, 0, 0]
+  var moisture = [
+    new five.Sensor({pin: "A0", freq: sensorCrawlInterval}),
+    new five.Sensor({pin: "A1", freq: sensorCrawlInterval}),
+    new five.Sensor({pin: "A2", freq: sensorCrawlInterval}),
+    new five.Sensor({pin: "A3", freq: sensorCrawlInterval}),
+    new five.Sensor({pin: "A4", freq: sensorCrawlInterval}),
+  ]
+  moisture.forEach((sensor, sensorId) => {
+    sensor.on('data', (data) => {
+      let low = 300;
+      let high = 600;
+      let percentage = Math.round((1 - (data-low)/(high-low)) * 1000) / 10
+      latestData[sensorId] = percentage
+    })
+  })
 
-  // This requires OneWire support using the ConfigurableFirmata
-  var thermometer = new five.Thermometer({
-    controller: "DS18B20",
-    pin: 2,
-    freq: 1000
-  });
+  setInterval(function() {
+    latestData.forEach((data, sensorId) => {
+      mqtt.publish(`greenhouse/moisture/soil/${sensorId}`, data.toString(), {retain: true})
+      //console.log(`${sensorId}: ${data}`)
+    })
+    let average = Math.round(latestData.reduce((pv, cr) => pv + cr, 0) / latestData.length * 10) / 10
+    mqtt.publish(`greenhouse/moisture/soil/average`, average.toString(), {retain: true})
+    //console.log(`average: ${average}`)
+  }, sensorCrawlInterval)
 
-  thermometer.on("data", function({celsius}) {
-    //console.log(`${celsius} celsius`)
-    //mqtt.publish(`greenhouse/temperature/outdoor`, celsius.toString())
-  });
-});*/
+
+});
